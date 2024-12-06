d6fb3ccecb92b32d78289049cbcd943472edefa1
tripathi74 <tripathishreya1108@gmail.com>
2024-12-06 16:00:00 +0530

    some change

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL =
   // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
